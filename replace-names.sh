3e5df9a6bb95891bf88e7b1a488ee3376515dd62
Sander Van Dooren <sander.vandooren@gmail.com>
2018-02-08 17:32:58 +0100

    Issue #OPENEUROPA-207: Rename package.

--- replace-names.sh
+++ replace-names.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 repo_root=$(git rev-parse --show-toplevel);
 repo_name=$(basename ${repo_root});
-subst="s/SKELETON/${repo_name}/g";
+subst="s/module-skeleton/${repo_name}/g";
 find ${repo_root} -maxdepth 1 -type f | xargs sed -i  ${subst};
 sed -i "/replace-names/d" composer.json
 rm replace-names.sh
