65a245578c4ded9b3847344db7db96c1e2f4c647
Sander Van Dooren <sander.vandooren@gmail.com>
2018-02-09 09:31:40 +0100

    Issue #OPENEUROPA-207: Scaffold module info file.

--- replace-names.sh
+++ replace-names.sh
@@ -3,5 +3,6 @@ repo_root=$(pwd);
 repo_name=$(basename ${repo_root});
 subst="s/module-skeleton/${repo_name}/g";
 find ${repo_root} -maxdepth 1 -type f | xargs sed -i  ${subst};
+mv module-skeleton.info.yml ${repo_name}.info.yml
 sed -i "/replace-names/d" composer.json
 rm replace-names.sh
