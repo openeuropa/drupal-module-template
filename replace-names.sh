f0fce178115f719c8f664fb1cf3664f216d42ba9
voidtek <vdacosta@voidtek.com>
2018-02-26 15:39:15 +0100

    OPENEUROPA-207: Change the travis yml.
    OPENEUROPA-207: Change the travis yml.

--- replace-names.sh
+++ replace-names.sh
@@ -4,5 +4,6 @@ repo_name=$(basename ${repo_root});
 subst="s/drupal-module-template/${repo_name}/g";
 find ${repo_root} -maxdepth 1 -type f | xargs sed -i  ${subst};
 mv drupal-module-template.info.yml ${repo_name}.info.yml
+mv .travis.module.yml ${repo_name}.info.yml
 sed -i "/replace-names/d" composer.json
 rm replace-names.sh
