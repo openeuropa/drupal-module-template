a6056f48ab871c49bbaeed8eb00f4f353e67657f
voidtek <vdacosta@voidtek.com>
2018-02-27 11:34:59 +0100

    OPENEUROPA-207: Improve the documentation.

--- replace-names.sh
+++ replace-names.sh
@@ -2,8 +2,16 @@
 repo_root=$(pwd);
 repo_name=$(basename ${repo_root});
 subst="s/drupal-module-template/${repo_name}/g";
+
+# Replace all occurrences of `drupal-module-template` with the module name.
 find ${repo_root} -maxdepth 1 -type f | xargs sed -i  ${subst};
+
+# Rename the info file.
 mv drupal-module-template.info.yml ${repo_name}.info.yml
+
+# Replace the travis.yml with travis.module.yml
 mv .travis.module.yml .travis.yml
+
+# Remove the call of replace-names.sh in composer and delete it
 sed -i "/replace-names/d" composer.json
 rm replace-names.sh
